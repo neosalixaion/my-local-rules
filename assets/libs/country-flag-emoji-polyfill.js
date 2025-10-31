f4ad3722eaa09490b7e3c1925a79a7220e096e91
neosalixaion <85350550+neosalixaion@users.noreply.github.com>
2025-10-31 12:09:20 +0300

    Add country-flag-emoji-polyfill library

---
+++ assets/libs/country-flag-emoji-polyfill.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - country-flag-emoji-polyfill@0.1.8
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/country-flag-emoji-polyfill
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/country-flag-emoji-polyfill@v0.1.8-elq2aWPlqxWw0DRaKldD/mode=imports/optimized/country-flag-emoji-polyfill.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/country-flag-emoji-polyfill@v0.1.8-elq2aWPlqxWw0DRaKldD/mode=imports,min/optimized/country-flag-emoji-polyfill.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/country-flag-emoji-polyfill@v0.1.8-elq2aWPlqxWw0DRaKldD/dist=es2019,mode=imports/optimized/country-flag-emoji-polyfill.js';
+export {default} from '/-/country-flag-emoji-polyfill@v0.1.8-elq2aWPlqxWw0DRaKldD/dist=es2019,mode=imports/optimized/country-flag-emoji-polyfill.js';
